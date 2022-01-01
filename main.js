d55a61cd08d9f89bac257cb1801c0f6d5a7f4089
asthajha0211 <asthajha05@gmail.com>
2022-01-01 23:47:20 +0700

    chore: change picture, add name js

---
+++ main.js
@@ -0,0 +1,8 @@
+function nameSwitch(){
+    var x= document.getElementById("name");
+    if (x.innerHTML=="Astha Jha") {
+        x.innerHTML= "Asstha Jha";
+    } else {
+        x.innerHTML="Astha Jha";
+    }
+}
